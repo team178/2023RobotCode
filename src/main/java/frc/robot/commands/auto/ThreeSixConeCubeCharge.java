91396da17be296bf3db634fa9537e96c034fb5ae
178Laptop <programming@farmingtonrobotics.org>
2023-03-15 18:54:27 -0400

    Potential new path, may not have enough time in auto and might be a bad angle

---
+++ src/main/java/frc/robot/commands/auto/ThreeSixConeCubeCharge.java
@@ -0,0 +1,91 @@
+package frc.robot.commands.auto;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.ArmPosition;
+import frc.robot.commands.Autos;
+import frc.robot.commands.DriveTrajectory;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Claw;
+import frc.robot.subsystems.Drivetrain;
+
+public class ThreeSixConeCubeCharge extends AutoCommand {
+
+    private Trajectory toCubeBlue;
+    private Trajectory toGridBlue;
+
+    private Trajectory toCubeRed;
+    private Trajectory toGridRed;
+
+    private Trajectory toChargeBlue;
+    private Trajectory toChargeRed;
+
+    @Override
+    public Pose2d getStartPosition() {
+        return getCubeTrajectory().getInitialPose();
+    }
+
+    private Trajectory getCubeTrajectory() {
+        if (DriverStation.getAlliance().equals(Alliance.Red)) {
+            return toCubeRed;
+        } else {
+            return toCubeBlue;
+        }
+    }
+
+    private Trajectory getGridTrajectory() {
+        if (DriverStation.getAlliance().equals(Alliance.Red)) {
+            return toGridRed;
+        } else {
+            return toGridBlue;
+        }
+    }
+
+    private Trajectory getChargeTrajectory() {
+        if (DriverStation.getAlliance().equals(Alliance.Red)) {
+            return toChargeRed;
+        } else {
+            return toChargeBlue;
+        }
+    }
+
+    public ThreeSixConeCubeCharge(Arm arm, Claw claw, Drivetrain drivetrain) {
+
+        toCubeBlue = PathPlanner.loadPath("36DriveToCube", new PathConstraints(1.75, 5), true);
+        toCubeRed = Autos.mirrorTrajectory(toCubeBlue);
+
+        toGridBlue = PathPlanner.loadPath("36DriveToGrid", new PathConstraints(2,5));
+        toGridRed = Autos.mirrorTrajectory(toGridBlue);
+
+        toChargeBlue = PathPlanner.loadPath("GridToCharge", new PathConstraints(2, 4.5));
+        toChargeRed = Autos.mirrorTrajectory(toChargeBlue);
+
+        this.addCommands(
+            Autos.placeHigh(arm, claw),
+            new WaitCommand(0.2),
+            Commands.parallel(
+                new DriveTrajectory(drivetrain, this::getCubeTrajectory),
+                Commands.sequence(
+                    new WaitCommand(0.7),
+                    arm.setPosition(ArmPosition.BACK),
+                    claw.open()
+                )
+            ),
+            claw.close(),
+            new WaitCommand(0.3),
+            arm.setPosition(ArmPosition.HOME),
+            new DriveTrajectory(drivetrain, this::getGridTrajectory),
+            Autos.placeHigh(arm, claw),
+            new WaitCommand(0.2),
+            new DriveTrajectory(drivetrain, this::getChargeTrajectory)
+        );
+    }
+
+}
