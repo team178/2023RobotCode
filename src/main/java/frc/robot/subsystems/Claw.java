3c8dcfd3dfe1e8e3782c75e26e2b8e4c804a99f8
Patrick Brennan <52932656+AM2i9@users.noreply.github.com>
2023-03-23 15:27:33 +0000

    switch ultrasonic to AnalogPotentiometer

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -10,14 +10,14 @@ import frc.robot.Constants.ClawConstants;
 
 public class Claw extends SubsystemBase {
     private Solenoid m_solenoid = new Solenoid(PneumaticsModuleType.CTREPCM, ClawConstants.kChannel);
-    private PWM m_ultrasonic = new PWM(1);
+    private AnalogPotentiometer m_ultrasonic = new AnalogPotentiometer(0, 6);
 
     public Claw() {
         m_solenoid.set(false);
     }
 
     public double getUltrasonicDistance() {
-        return m_ultrasonic.getRaw();
+        return m_ultrasonic.get();
     }
 
     public void toggle() {
