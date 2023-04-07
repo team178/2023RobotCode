4575008e1d345ac625568f9916dbcaadad4ecc99
178Laptop <programming@farmingtonrobotics.org>
2023-04-07 09:41:01 -0400

    Make lower arm 'back' position lower

--- src/main/java/frc/robot/commands/ArmPosition.java
+++ src/main/java/frc/robot/commands/ArmPosition.java
@@ -7,7 +7,7 @@ public enum ArmPosition {
     SUBSTATION(4.093430, 5.156882),
     HIGH (2.976432, 3.154062),
     LOW (3.587507, 4.466632),
-    BACK (5.407518, 3.051937);
+    BACK (5.407518, 2.951937);
 
     public double lower;
     public double upper;
